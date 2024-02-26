2e525c373d96e684f525d99a86bbd7ffe31dc407
David Hurley <dhurls99@gmail.com>
2024-02-26 10:43:39 -0500

    remove type causing deploy issue

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,7 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import '@nomicfoundation/hardhat-toolbox-viem';
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+const config = {
+  solidity: '0.8.24',
 };
 
 export default config;
