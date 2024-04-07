5b291d4bdbb2fcd19637994c994dd5e32c1fc180
David Hurley <dhurls99@gmail.com>
2024-04-07 17:57:12 -0400

    deploy without contract deploy script

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,18 +1,18 @@
-import { ethers } from 'hardhat';
+// import { ethers } from 'hardhat';
 
-async function main() {
-  const degenAddress = '0xfee293840d23b0b2de8c55e1cf7a9f01c157767c';
+// async function main() {
+//   const degenAddress = '0xfee293840d23b0b2de8c55e1cf7a9f01c157767c';
 
-  // const degenAddress = '0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed';
+//   // const degenAddress = '0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed';
 
-  const hodlem = await ethers.deployContract('Hodlem', [degenAddress]);
+//   const hodlem = await ethers.deployContract('Hodlem', [degenAddress]);
 
-  console.log(`hodlem deployed to ${hodlem.target}`);
-}
+//   console.log(`hodlem deployed to ${hodlem.target}`);
+// }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
